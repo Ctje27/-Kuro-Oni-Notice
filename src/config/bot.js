39ebb284bfccb7bc2f890bd9664e97d8946aaff6
Ctje27 <black.cenni@gmail.com>
2026-07-16 18:16:19 +0200

    Change activity type from 4 to 0 in bot config

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "", // required by Discord API, not shown in the client
         state: "",     // this is what people actually see
-        type: 4,               // Custom
+        type: 0,               // Custom
       },
     ],
   },
